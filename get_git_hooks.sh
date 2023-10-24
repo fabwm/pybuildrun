b21b1b07e372c0802cd93cb43e86b38ac4d49da7
fabiano <fwm_95@hotmail.com>
2023-10-24 11:10:28 -0300

    update pybuildrun, change pipeline config name

--- get_git_hooks.sh
+++ get_git_hooks.sh
@@ -9,21 +9,18 @@ function check_dir()
         if [ -d $1 ]; then
             cd $1 && git log 2> /dev/null 1> /dev/null
             if [ $? != 0 ]; then
-                echo "Not a git repository - please input a git repo dir as a arg  - $(date)" # >> log.txt
+                echo "Not a git repository - please input a git repo dir as a arg  - $(date)"  >> log.txt
                 exit 1
             else
                 get_commit_logs $1
             fi
-            #echo $1
         else
             echo "diretorio $1 invalido - $(date)" >> log.txt
-            #echo "diretorio $1 invalido - $(date)"
         fi
     
     else
     
         echo "no args - $(date)" >> log.txt
-        #echo "no args - $(date)"
     
     fi
 }
@@ -34,8 +31,7 @@ function get_commit_logs()
     COMMIT_DONE=$(git log | grep commit | cut -d " " -f 2)
     ARRAY_COMMIT_HASH=($COMMIT_DONE)
     for (( i=0; i<=${#ARRAY_COMMIT_HASH[@]}; i++ )); do
-        GIT_SHOW=$(git show ${ARRAY_COMMIT_HASH[$i]}) # | grep diff ) | cut -d "/" -f 3)
-        #GIT_SHOW=$(git show ${ARRAY_COMMIT_HASH[$i]} | grep file )
+        GIT_SHOW=$(git show ${ARRAY_COMMIT_HASH[$i]}) 
     done
 }
 
--- pybuildrun.py
+++ pybuildrun.py
@@ -15,7 +15,6 @@ def monitor_commit():
         git_repo = sys.argv[1]
         commit_file = os.popen(f"./get_git_hooks.sh {git_repo}; echo $?").read()
         get_error = commit_file.split()[-1]
-        #print(get_error)
     
     if get_error == '1':
         print(f"{commit_file}^exit code")
@@ -34,10 +33,9 @@ def monitor_commit():
 
 def get_json_pipeline():
     
-    pipeline = os.popen("cat ci_json.json").read()
+    pipeline = os.popen("cat pipeline_config.json").read()
     json_pipeline = json.loads(pipeline)
     
-    #print(json_pipeline)
     return json_pipeline
 
 def validate_pipeline(json_pipeline):
@@ -49,7 +47,7 @@ def validate_pipeline(json_pipeline):
             if "run" in json_pipeline[x]:
                 for y in json_pipeline[x]:
                     if y == "run":
-                        pass
+                        return True
                     else:
                         print("invalid key value in pipeline, please erase all keys in pipeline that is not 'run'")
                         return False
@@ -64,16 +62,20 @@ def run_pipeline(pipeline):
 
     commands = pipeline["pipeline"]["run"]
     for key in commands:
-        #print(commands[key])
+    
         step_run = os.popen(f"{commands[key]} 2> /dev/null; echo $?").read()
         step_error = step_run.split()[-1]
+        
         if step_error != "0":
-            print(f"not work {commands[key]}")
+            print(f"command {commands[key]} error with exit {step_error}")
+            os.popen(f'echo "command {commands[key]} error with exit {step_error} - $(date)" >> log.txt')
             break
         else:
-            print(step_run)
+            pass
 
 #monitor_commit()
-pipeline = get_json_pipeline()
-#validate_pipeline(pipeline)
-run_pipeline(pipeline)
+if __name__ == '__main__':
+    pipeline = get_json_pipeline()
+    validate = validate_pipeline(pipeline)
+    if validate == True:
+        run_pipeline(pipeline)

6312a1d5931dc729055de3a2b5e0de7ca4b9da4b
fabiano <fwm_95@hotmail.com>
2023-12-11 23:14:21 -0300

    coment git trigger for they may not be used anymore

--- pybuildrun.py
+++ pybuildrun.py
@@ -12,52 +12,52 @@ script_init ="\
 ██║░░░░░░░░██║░░░██████╦╝╚██████╔╝██║███████╗██████╔╝██║░░██║╚██████╔╝██║░╚███║"
 
 
-def git_trigger(json_pipeline):
+# def git_trigger(json_pipeline):
 
-    try:
-        git_repo = sys.argv[2]
-    except:
-        print("Script call error...\nPass the git repo directory as a arg to run the pipeline")
-        exit()
-    else:
-        git_repo = sys.argv[2]
-        commit_file = os.popen(f"./get_git_hooks.sh {git_repo}; echo $?").read()
-        get_error = commit_file.split()[-1]
+#     try:
+#         git_repo = sys.argv[2]
+#     except:
+#         print("Script call error...\nPass the git repo directory as a arg to run the pipeline")
+#         exit()
+#     else:
+#         git_repo = sys.argv[2]
+#         commit_file = os.popen(f"./get_git_hooks.sh {git_repo}; echo $?").read()
+#         get_error = commit_file.split()[-1]
     
-    if get_error == '1':
-        print(f"{commit_file}^Exit code\nCheck log file to see the error...")
-    else:
-        print("*******************************************************************************\n\n")
-        print(script_init)
-        print("\n\n*******************************************************************************")
-        print("\n\nStarting pybuildrun....\nWaiting for commits\n\n")
-        while True:
+#     if get_error == '1':
+#         print(f"{commit_file}^Exit code\nCheck log file to see the error...")
+#     else:
+#         print("*******************************************************************************\n\n")
+#         print(script_init)
+#         print("\n\n*******************************************************************************")
+#         print("\n\nStarting pybuildrun....\nWaiting for commits\n\n")
+#         while True:
             
-            last_commit_file = os.popen(f"./get_git_hooks.sh {git_repo}; echo $?").read()
-            if last_commit_file == commit_file:
-                pass
-            else:
-                print("Commit received...\nStarting pipeline...")
-                pipeline_run_status = run_pipeline(json_pipeline)
-                commit_file = last_commit_file
-                if pipeline_run_status == False:
-                    print("Pipeline error, one or more commands within key 'run:{}' are not working")
-                    exit()
-                else:
-                    print("Pipeline succesfull run\n")
-                    var_for_validation_inloop = 0
-                    user_decision_to_continue_run_pipeline = input("Would you like to continue listening to git commits? (y/n)\n")
-                    while(var_for_validation_inloop == 0):
-                        if user_decision_to_continue_run_pipeline == 'y':
-                            var_for_validation_inloop = 1
-                            print("\nRestarting pybuildrun...\nWaiting for commits\n")
-                            pass
-                        elif user_decision_to_continue_run_pipeline == 'n':
-                            print("\nExiting pybuildrun...")
-                            exit()
-                        else:
-                            print("Not readable answer, please input 'y' or 'n' to continue pybuildrun.\n")
-                            pass
+#             last_commit_file = os.popen(f"./get_git_hooks.sh {git_repo}; echo $?").read()
+#             if last_commit_file == commit_file:
+#                 pass
+#             else:
+#                 print("Commit received...\nStarting pipeline...")
+#                 pipeline_run_status = run_pipeline(json_pipeline)
+#                 commit_file = last_commit_file
+#                 if pipeline_run_status == False:
+#                     print("Pipeline error, one or more commands within key 'run:{}' are not working")
+#                     exit()
+#                 else:
+#                     print("Pipeline succesfull run\n")
+#                     var_for_validation_inloop = 0
+#                     user_decision_to_continue_run_pipeline = input("Would you like to continue listening to git commits? (y/n)\n")
+#                     while(var_for_validation_inloop == 0):
+#                         if user_decision_to_continue_run_pipeline == 'y':
+#                             var_for_validation_inloop = 1
+#                             print("\nRestarting pybuildrun...\nWaiting for commits\n")
+#                             pass
+#                         elif user_decision_to_continue_run_pipeline == 'n':
+#                             print("\nExiting pybuildrun...")
+#                             exit()
+#                         else:
+#                             print("Not readable answer, please input 'y' or 'n' to continue pybuildrun.\n")
+#                             pass
 
 def get_json_pipeline():
     
@@ -80,9 +80,8 @@ def get_json_pipeline():
 def validate_pipeline(json_pipeline):
 
     for x in json_pipeline:
-        if x == "trigger":
-            pass
-        elif x == "pipeline":
+        
+        if x == "pipeline":
             if "run" in json_pipeline[x]:
                 for y in json_pipeline[x]:
                     if y == "run":
@@ -108,7 +107,7 @@ def run_pipeline(pipeline):
         step_error = step_run.split()[-1]
         
         if step_error != "0":
-            print(f"Command {commands[key]} error with exit {step_error}\ncheck logfile to see the error")
+            print(f"Command {commands[key]} error with exit {step_error}\ncheck logfile to see the error\n")
             os.popen(f'echo $(date)" >> log_pybuildrun.txt')
             return False
             break
@@ -117,39 +116,42 @@ def run_pipeline(pipeline):
     
     return True
 
-def get_trigger():
+# def get_trigger():
+    
+#     json_pipeline = get_json_pipeline()
+#     pipeline = validate_pipeline(json_pipeline)
+
+#     trigger = pipeline["trigger"]
+
+#     return trigger, pipeline
+
+def app_run():
     
     json_pipeline = get_json_pipeline()
     pipeline = validate_pipeline(json_pipeline)
-
-    trigger = pipeline["trigger"]
-
-    return trigger, pipeline
-
-def manual_trigger(pipeline):
-    
     print("*******************************************************************************\n\n")
     print(script_init)
     print("\n\n*******************************************************************************")
-    print("\n\nStarting pybuildrun....\nrunning pipeline manually\n\n")
+    print("\n\nStarting pybuildrun....\nrunning pipeline\n\n")
     time.sleep(1)
     run_pipeline(pipeline)
     print("Done")
 
 
-def app_run():
+# def app_run():
 
-    trigger = get_trigger()
-    pipeline = trigger[1]
+    # trigger = get_trigger()
+    # pipeline = trigger[1]
 
-    if trigger[0] == 'git':
-        git_trigger(pipeline)
-    elif trigger[0] == 'manual':
-        manual_trigger(pipeline)
-    else:
-        print("Pipeline error: select trigger in pipeline between 'manual' or 'git'")
+    # if trigger[0] == 'git':
+    #     git_trigger(pipeline)
+    # elif trigger[0] == 'manual':
+    # manual_trigger()
+    # else:
+    #     print("Pipeline error: select trigger in pipeline between 'manual' or 'git'")
 
 
 if __name__ == '__main__':
+    os.chdir('/home/fabiano')
     app_run()
     #print(get_json_pipeline())
